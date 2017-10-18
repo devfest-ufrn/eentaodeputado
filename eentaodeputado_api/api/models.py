e80a9bb8126f169c4a12b91801f526cc00a4ee33
PCoelho07 <pedro_coelho_pv@hotmail.com>
2017-10-18 17:49:29 -0300

    Api da aplicação adicionada

---
+++ eentaodeputado_api/api/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Deputado(models.Model):
+
+	def __init__(self, arg):
+		pass
+		
+
