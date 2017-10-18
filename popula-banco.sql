6f2569af8dcb394c0304f8d062a08d737b046286
PCoelho07 <pedro_coelho_pv@hotmail.com>
2017-10-18 16:20:53 -0300

    Client da aplicação adicionado

--- popula-banco.sql
+++
@@ -1,9 +0,0 @@
-use eentaodeputado;
-
-load data local infile '/data/deputados.csv' into table deputado fields terminated by ','
-  enclosed by '"'
-  lines terminated by '\n'
-    (ideCadastro, codOrcamento, condicao,	
-    	matricula, idParlamentar, nome, nomeParlamentar,urlFoto, sexo, uf, partido, gabinete, anexo, fone, email, comissoes);
-
-
